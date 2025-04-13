c5234349efc03ab87581af17873944721520f4bc
puchacz1337 <jakub.bienkowski@teb.edu.pl>
2025-04-13 18:44:10 +0200

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+      
+    }
+}
